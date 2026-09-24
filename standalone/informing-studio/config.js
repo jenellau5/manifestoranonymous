4b216394794ad0f8f594ee2f832f461abe8a4b22
jenellau5 <jenellmulitalo@gmail.com>
2026-09-23 22:52:45 -0600

    Set Informing Studio password and room-owner coupon

--- standalone/informing-studio/config.js
+++ standalone/informing-studio/config.js
@@ -1,6 +1,6 @@
 // EDIT THESE THREE VALUES. This browser-side gate is not secure purchase verification.
 window.MA_STANDALONE = Object.freeze({
-  password: "CHANGE-ME-INFORMING-PASSWORD",
-  couponCode: "REPLACE_WITH_YOUR_ROOM_OWNER_COUPON",
+  password: "INFORMNOW26",
+  couponCode: "MAPLAYGROUND10",
   playgroundUrl: "https://payhip.com/b/4SZio"
 });
